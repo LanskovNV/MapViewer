7adaf9de73404c329ea86bfdeda217ef80c1821d
LanskovNV <leins275@gmail.com>
2019-03-10 12:42:05 +0300

    Updated map page and increased max width of content

--- app/containers/MapPage/index.js
+++ app/containers/MapPage/index.js
@@ -2,8 +2,16 @@
  * MapPage
  */
 import React from 'react';
+import styled from 'styled-components';
+
+import H3 from '../../components/H3';
 import Img from '../../components/Img';
 import blank from '../../images/MapViewBlank.png';
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 export default class MapPage extends React.Component {
   shouldComponentUpdate() {
@@ -11,6 +19,16 @@ export default class MapPage extends React.Component {
   }
 
   render() {
-    return <Img src={blank} alt="MapView" />;
+    return (
+      <div>
+        <Wrapper>
+          <Img src={blank} alt="MapView" />
+          <H3> Map data </H3>
+        </Wrapper>
+        <Wrapper>
+          <div />
+        </Wrapper>
+      </div>
+    );
   }
 }

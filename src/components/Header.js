ba2c7d62eaee0db369c42580ab94ae49680be8af
sevarsh <uchebnyivarsh@gmail.com>
2019-03-27 13:58:22 +0300

    Add new header with app logo
    New header with a new logo was added.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-import messages from '../messages';
+import { Badge, Card } from 'react-bootstrap';
 
 class Header extends React.Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <Card className="text-center">
+          <Card.Body>
+            <Card.Title>
+              {' '}
+              <h1>
+                Map <Badge variant="info">Viewer</Badge>
+              </h1>{' '}
+            </Card.Title>
+          </Card.Body>
+        </Card>
+      </div>
+    );
   }
 }
 

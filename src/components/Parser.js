36a844470c29c6601dedadfc38ba7a044df9da79
Tumanov Viatcheslav <tslavich@bk.ru>
2019-04-05 14:11:49 +0300

    Static Load Commit
    -Move readyMaps to src
    -Add static loading

--- src/components/Parser.js
+++ src/components/Parser.js
@@ -1,23 +1,56 @@
 import { injectIntl } from 'react-intl';
+import mapFile1 from '../readyMaps/Davis.osm.geojson';
+import mapFile2 from '../readyMaps/Alexandria.osm.geojson';
+import mapFile3 from '../readyMaps/Cairo.osm.geojson';
 
 class Parser {
+  static LoadPreparedMap(e) {
+    let file;
+    let name = e.target.id;
+    console.log(name);
+    if (name === 'preloadMap1') {
+      file = mapFile1;
+    } else if (name === 'preloadMap2') {
+      file = mapFile2;
+    } else if (name === 'preloadMap3') {
+      file = mapFile3;
+    }
+    fetch(file)
+      .then(function(response) {
+        if (response.status !== 200) {
+          console.log(
+            'Looks like there was a problem. Status Code: ' + response.status
+          );
+          return;
+        }
+
+        // Examine the text in the response
+        response.arrayBuffer().then(function(data) {
+          // TODO Have ArrayBuffer, need to process it
+          alert('File size: ' + data.byteLength);
+        });
+      })
+      .catch(function(err) {
+        console.log('Fetch Error :-S', err);
+      });
+  }
   static PickUsefulFromGeoJSONToTXT() {
     const FIRST_ELEMENT = 0;
     const file = document.getElementById('loadedMap').files[FIRST_ELEMENT];
 
-    loading(
-      file,
-      function(data) {
-        // TODO Add data processing
-        if (data === null) {
-          alert(data);
-        }
-      },
-      function() {
-        alert('End. All data successfully read');
-      }
-    );
+    loading(file, callbackDataProcess, callbackEnd);
   }
+}
+
+function callbackDataProcess(data) {
+  // TODO Add data processing
+  if (data === null) {
+    alert(data);
+  }
+}
+
+function callbackEnd() {
+  alert('End. All data successfully read');
 }
 
 function min(a, b) {

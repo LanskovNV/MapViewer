cc7ed4b72d813957966cda73b5f8d7060591ddaf
Tumanov Viatcheslav <tslavich@bk.ru>
2019-04-05 14:27:57 +0300

    lint Commit
    -Fix code according to lint errors

--- src/components/Parser.js
+++ src/components/Parser.js
@@ -6,8 +6,7 @@ import mapFile3 from '../readyMaps/Cairo.osm.geojson';
 class Parser {
   static LoadPreparedMap(e) {
     let file;
-    let name = e.target.id;
-    console.log(name);
+    const name = e.target.id;
     if (name === 'preloadMap1') {
       file = mapFile1;
     } else if (name === 'preloadMap2') {
@@ -18,22 +17,21 @@ class Parser {
     fetch(file)
       .then(function(response) {
         if (response.status !== 200) {
-          console.log(
-            'Looks like there was a problem. Status Code: ' + response.status
-          );
+          alert('Looks like there was a problem.');
           return;
         }
 
         // Examine the text in the response
         response.arrayBuffer().then(function(data) {
           // TODO Have ArrayBuffer, need to process it
-          alert('File size: ' + data.byteLength);
+          alert(data.byteLength);
         });
       })
       .catch(function(err) {
-        console.log('Fetch Error :-S', err);
+        alert(err);
       });
   }
+
   static PickUsefulFromGeoJSONToTXT() {
     const FIRST_ELEMENT = 0;
     const file = document.getElementById('loadedMap').files[FIRST_ELEMENT];
@@ -57,14 +55,12 @@ function min(a, b) {
   return a < b ? a : b;
 }
 
-function loading(file, callbackProgress, callbackEnd) {
+function loading(file, callbackProgressF, callbackEndF) {
   const CHUNK_SIZE = 10 * 1024;
   let start = 0;
   let end;
   let slice;
-  const extension = file.name.slice(
-    ((file.name.lastIndexOf('.') - 1) >>> 0) + 2
-  );
+  const extension = file.name.slice(file.name.lastIndexOf('.') - 1 + 2);
   if (file === undefined) {
     return;
   }
@@ -73,7 +69,7 @@ function loading(file, callbackProgress, callbackEnd) {
     return;
   }
   if (file.size === 0) {
-    callbackEnd();
+    callbackEndF();
   }
   while (start < file.size) {
     end = min(start + CHUNK_SIZE, file.size);
@@ -82,17 +78,17 @@ function loading(file, callbackProgress, callbackEnd) {
     reader.size = CHUNK_SIZE;
     reader.offset = start;
     reader.onload = function(evt) {
-      callbackRead(this, file, evt, callbackProgress, callbackEnd);
+      callbackRead(this, file, evt, callbackProgressF, callbackEndF);
     };
     reader.readAsArrayBuffer(slice);
     start += CHUNK_SIZE;
   }
 }
 
-function callbackRead(reader, file, evt, callbackProgress, callbackEnd) {
-  callbackProgress(evt.target.result);
+function callbackRead(reader, file, evt, callbackProgressF, callbackEndF) {
+  callbackProgressF(evt.target.result);
   if (reader.offset + reader.size >= file.size) {
-    callbackEnd();
+    callbackEndF();
   }
 }
 

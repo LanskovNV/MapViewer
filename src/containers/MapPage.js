ded76a900e8745e0781664290ac353ce373ed135
sevarsh <uchebnyivarsh@gmail.com>
2019-04-09 19:07:27 +0300

    Add CheckboxBar component
    * CheckboxBar component was created
    * Button Toolbar was removed because it was useless
    * Checkboxes were moved from MapPage to the new CheckboxBar component
    * The new component was added to page in app.js

--- src/containers/MapPage.js
+++ src/containers/MapPage.js
@@ -2,11 +2,9 @@
  * MapPage
  */
 import React from 'react';
-import { Button, ButtonGroup, InputGroup } from 'react-bootstrap';
-import { FormattedMessage, injectIntl } from 'react-intl';
+import { injectIntl } from 'react-intl';
 import styled from 'styled-components';
 import blank from './MapViewBlank.png';
-import messages from '../messages';
 
 const Wrapper = styled.div`
   display: flex;
@@ -23,58 +21,7 @@ class MapPage extends React.Component {
       <div>
         <Wrapper>
           <img src={blank} alt="MapView" />
-          <div>
-            <h2>
-              <FormattedMessage {...messages.Info} />
-            </h2>
-            <h3>
-              <FormattedMessage {...messages.Flags} />
-            </h3>
-            <InputGroup className="mb-3">
-              <InputGroup.Prepend>
-                <InputGroup.Checkbox style={{ marginLeft: 10 }} />
-                <InputGroup.Text style={{ marginLeft: 10 }}>
-                  <FormattedMessage {...messages.Streets} />
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <InputGroup.Prepend>
-                <InputGroup.Checkbox style={{ marginLeft: 10 }} />
-                <InputGroup.Text style={{ marginLeft: 10 }}>
-                  <FormattedMessage {...messages.Houses} />
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-              <InputGroup.Prepend>
-                <InputGroup.Checkbox style={{ marginLeft: 10 }} />
-                <InputGroup.Text style={{ marginLeft: 10 }}>
-                  <FormattedMessage {...messages.Water} />
-                </InputGroup.Text>
-              </InputGroup.Prepend>
-            </InputGroup>
-          </div>
         </Wrapper>
-        <div align="center">
-          <ButtonGroup
-            style={{
-              marginTop: 30,
-              marginBottom: 30
-            }}
-          >
-            <Button variant="info">+</Button>
-            <Button variant="info">-</Button>
-            <Button variant="info">
-              <FormattedMessage {...messages.Up} />
-            </Button>
-            <Button variant="info">
-              <FormattedMessage {...messages.Down} />
-            </Button>
-            <Button variant="info">
-              <FormattedMessage {...messages.Left} />
-            </Button>
-            <Button variant="info">
-              <FormattedMessage {...messages.Right} />
-            </Button>
-          </ButtonGroup>
-        </div>
       </div>
     );
   }

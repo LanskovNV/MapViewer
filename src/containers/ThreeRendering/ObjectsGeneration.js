e0ab5317bd227cd283d0489fc91e87f2e277ee6e
LanskovNV <Krasavchik4427>
2019-08-21 00:29:11 +0300

    Refactored
     - save colors in Object generation
     - create mesh and lines materials in Draw "online"

--- src/containers/ThreeRendering/ObjectsGeneration.js
+++ src/containers/ThreeRendering/ObjectsGeneration.js
@@ -1,5 +1,3 @@
-import * as THREE from 'three';
-
 export default function objectGeneration(objects) {
   const elems = [];
   const houses = {
@@ -7,21 +5,21 @@ export default function objectGeneration(objects) {
     name: 'houses',
     toDraw: objects.isHouses,
     data: document.getElementById('housesProcFile'),
-    material: new THREE.MeshBasicMaterial({ color: '#520' })
+    color: '#520'
   };
   const streets = {
     id: 0,
     name: 'streets',
     toDraw: objects.isStreets,
     data: document.getElementById('streetsProcFile'),
-    material: new THREE.MeshBasicMaterial({ color: '#E90' })
+    color: '#E90'
   };
   const water = {
     id: 0,
     name: 'water',
     toDraw: objects.isWater,
     data: document.getElementById('waterProcFile'),
-    material: new THREE.MeshBasicMaterial({ color: '#0AF' })
+    color: '#0AF'
   };
   elems.push(houses, streets, water);
   return elems;

84979f006106d87d7062c156b1e789dbd53b9f0e
leins275 <feitrich@yandex.ru>
2019-09-30 14:05:36 +0300

    Added flattenCoords func
     - preparing geojson polygon to earcut alg

---
+++ src/containers/Triangulation/flatten.js
@@ -0,0 +1,18 @@
+export default flattenCoords(data);
+{
+  const dim = data[0][0].length;
+  const result = { vertices: [], holes: [], dimensions: dim };
+  let holeIndex = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    for (let j = 0; j < data[i].length; j++) {
+      for (let d = 0; d < dim; d++) result.vertices.push(data[i][j][d]);
+    }
+    if (i > 0) {
+      holeIndex += data[i - 1].length;
+      result.holes.push(holeIndex);
+    }
+  }
+
+  return result;
+}

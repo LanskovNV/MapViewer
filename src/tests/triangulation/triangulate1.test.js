c267da3f030e0da9e4be87e2750841fe93857478
sevarsh <uchebnyivarsh@gmail.com>
2019-10-08 00:27:15 +0300

    Add triangulation testing
    * 4 new tests were added
    * 1 test was fixed

---
+++ src/tests/triangulation/triangulate1.test.js
@@ -0,0 +1,31 @@
+import { triangulate } from '../../containers/Triangulation/triangulation';
+import isCorrect from './isCorrect';
+
+test('triangulate 1', () => {
+  const geometry = [
+    [100, 100],
+    [100, 200],
+    [150, 150],
+    [200, 200],
+    [200, 100],
+    [100, 100]
+  ];
+  const correctResults = [
+    [
+      [[100, 100], [100, 200], [150, 150]],
+      [[100, 100], [150, 150], [200, 100]],
+      [[100, 100], [150, 150], [200, 100]]
+    ],
+    [
+      [[150, 150], [200, 200], [200, 100]],
+      [[100, 100], [100, 200], [200, 100]]
+    ],
+    [[[100, 100], [200, 200], [200, 100]], [[100, 100], [100, 200], [150, 150]]]
+  ];
+
+  //add the result of triangulation here
+  let triangles = triangulate(geometry);
+  console.log(triangles);
+
+  expect(isCorrect(triangles, correctResults)).toEqual(true);
+});
--- src/tests/triangulation/triangulate3.test.js
+++ src/tests/triangulation/triangulate3.test.js
@@ -1,17 +1,22 @@
 import { triangulate } from '../../containers/Triangulation/triangulation';
 import isCorrect from './isCorrect';
 
-test('triangulate 0', () => {
-  const geometry = [[100, 100], [100, 200], [200, 200], [200, 100], [100, 100]];
+test('triangulate 3', () => {
+  const geometry = [
+    [50, 0],
+    [50, 100],
+    [100, 50],
+    [150, 100],
+    [150, 0],
+    [100, 50],
+    [50, 0]
+  ];
   const correctResults = [
-    [
-      [[100, 100], [100, 200], [200, 200]],
-      [[100, 100], [200, 200], [200, 100]]
-    ],
-    [[[100, 200], [200, 200], [200, 100]], [[100, 100], [100, 200], [200, 100]]]
+    [[[50, 0], [50, 100], [100, 50]], [[100, 50], [150, 100], [150, 0]]]
   ];
 
   //add the result of triangulation here
+  //add prep?
   let triangles = triangulate(geometry);
   console.log(triangles);
 

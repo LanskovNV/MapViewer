7c3102981fd446fe4f270b5fe723d1ec8a8da08a
leins275 <feitrich@yandex.ru>
2019-10-29 01:11:47 +0300

    Fixed
     - removed crutch with linkedList

--- src/containers/Triangulation/triangulate.js
+++ src/containers/Triangulation/triangulate.js
@@ -1,9 +1,8 @@
 import { eliminateHoles } from './preprocessing';
-import { removeNode, insertNode } from './node';
+import { removeNode } from './node';
 import { filterPoints } from './preprocessing';
 import { equals } from './source';
 import {
-  signedArea,
   area,
   splitPolygon,
   pointInTriangle,
@@ -12,27 +11,7 @@ import {
   intersects
 } from './geometry';
 
-// import { linkedList } from './linkedList';
-
-// create a circular doubly linked list from polygon points in the specified winding order
-const linkedList = (data, start, end, dim, clockwise) => {
-  let i, last;
-
-  if (clockwise === signedArea(data, start, end, dim) > 0) {
-    for (i = start; i < end; i += dim)
-      last = insertNode(i, data[i], data[i + 1], last);
-  } else {
-    for (i = end - dim; i >= start; i -= dim)
-      last = insertNode(i, data[i], data[i + 1], last);
-  }
-
-  if (last && equals(last, last.next)) {
-    removeNode(last);
-    last = last.next;
-  }
-
-  return last;
-};
+import { linkedList } from './linkedList';
 
 const earcut = (data, holeIndices, dim) => {
   dim = dim || 2;
